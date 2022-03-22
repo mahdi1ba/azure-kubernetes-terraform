cdbde94e9d8c4b9b0d9fbe473669f455b7aa4398
mahdibenayed2 <47032423+mahdibenayed2@users.noreply.github.com>
2022-03-22 03:53:39 +0100

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -45,7 +45,7 @@ provider "kubernetes" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     exec {
         api_version = "client.authentication.k8s.io/user"
-        args = ["eks", "get-token", "--cluster-name", "in28minutes-cluster"]
+        args = ["eks", "get-token", "--cluster-name", "in28minutes-cluster","--region","us-east-1" ]
         command = "aws"
     }
     token = data.aws_eks_cluster_auth.cluster.token
