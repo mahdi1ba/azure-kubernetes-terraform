f9dbc2dc9da961850c2657ded08a166c2e70065d
mahdibenayed2 <47032423+mahdibenayed2@users.noreply.github.com>
2022-03-22 03:23:22 +0100

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -22,7 +22,7 @@ data "aws_subnet_ids" "subnets" {
 provider "kubernetes" {
     host = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token = data.aws_eks_cluster.cluster.token
+    #token = data.aws_eks_cluster.cluster.token
     load_config_file = false
     version = "~> 1.9"
 }
