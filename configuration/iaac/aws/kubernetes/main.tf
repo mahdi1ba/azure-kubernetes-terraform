be4e6e3585006407cd9cfe541b41d07379b0997d
mahdiben <benayedmahdi123@gmail.com>
2022-03-24 00:35:49 +0100

    main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,7 +1,8 @@
 
 
-# terraform-backend-state-1234
-#access key ID : AKIAW6RDS7DVFCQ7BMXB
+#terraform-backend-state-12
+#access key ID : AKIAYIEPAEP7AIJTKLQZ
+
 
 terraform {
     backend "s3" {
@@ -10,15 +11,11 @@ terraform {
         region = "us-east-1"
     }
 }
-
 resource "aws_default_vpc" "default" {
-
 }
-
 data "aws_subnet_ids" "subnets" {
     vpc_id = aws_default_vpc.default.id 
 }
-
 provider "kubernetes" {
     host = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
@@ -30,7 +27,7 @@ module "in28minutes-cluster" {
     source = "terraform-aws-modules/eks/aws"
     cluster_name="in28minutes-cluster"
     cluster_version="1.14"
-    subnets = ["subnet-0113a6634c96dcb29","subnet-0b0dc0b63f913e8b3"]
+    subnets = ["subnet-0f135821265bedbd4","subnet-0e2a35aebf9410682"]
     #subnets = data.aws_subnet_ids.subnets.ids
     vpc_id = aws_default_vpc.default.id
     #vpc_id = "vpc-1234556abcdef"
@@ -44,16 +41,14 @@ module "in28minutes-cluster" {
     ]
 }
 data "aws_eks_cluster" "cluster"{
-    name = module.my-cluster.cluster_id
+    name = module.in28minutes-cluster.cluster_id
 }
 data "aws_eks_cluster_auth" "cluster"{
-    name = module.my-cluster.cluster_id
+    name = module.in28minutes-cluster.cluster_id
 }
-
 #we will use serviceaccount to connect to k8s cluster in CI/CD mode
 # serviceaccount needs permissions to create deployments
 #and services in default namespace
-
 resource "kubernetes_cluster_role_binding" "exemple"{
     metadata {
       name = "fabric8-rbac"
